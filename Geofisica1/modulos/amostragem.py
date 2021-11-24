85ab8ea2e9907a999644ef7694a8af0fea1397d6
Emerson NoteUFU <emerson.almeida@ufu.br>
2021-11-24 11:29:36 -0300

    códigos refatorados para o notebook de amostragem

--- Geofisica1/modulos/amostragem.py
+++ Geofisica1/modulos/amostragem.py
@@ -39,10 +39,15 @@ def gera_sinal_analogico(amplitude, frequencia, t_window):
     
 def digitaliza_sinal(s_analog, f_am):
     dt = 1 / f_am
-    t_window = s_analog.t[-1] - s_analog.t[0]
-    n_amostras = int(t_window / dt) + 1
-    t_amostragem = np.linspace(s_analog.t[0], s_analog.t[-1], n_amostras, 
-                               endpoint=True)
+    t_amostragem = [0.0]
+    
+    while t_amostragem[-1] < (s_analog.t[-1] - dt):
+        t_amostragem.append(t_amostragem[-1] + dt)
+    
+    #t_window = s_analog.t[-1] - s_analog.t[0]
+    #n_amostras = np.int(np.round(t_window / dt, 0)) + 1
+    #t_amostragem = np.linspace(s_analog.t[0], s_analog.t[-1], n_amostras, 
+    #                           endpoint=True)
     
     # usar o interp1d funciona melhor do que usando o resample
     f_digitaliza = interp1d(s_analog.t, s_analog.y, kind='linear')
@@ -127,161 +132,63 @@ def executa_exercício3(amplitude, frequencia, t_window, fam):
     return None
 
 
+def executa_exercício4(amplitude, frequencia, t_window):
+    y = []
 
+    for a, f in zip(amplitude, frequencia):
+        y.append(gera_sinal_analogico(a, f, t_window))
 
+    sinal_analog = gera_sinal_analogico(0.0, 0.0, t_window)
+    sinal_analog.y = y[0].y + y[1].y + y[2].y + y[3].y + y[4].y
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ----------------------------------
-
-class sinal_monofreq():
+    fig, ax = plt.subplots(2, 1, figsize=(15, 6), sharex=True)
+    cores = ['red', 'black', 'green', 'cyan', 'magenta']
+    labels = ['$y_1(t)$', '$y_2(t)$', '$y_3(t)$', '$y_4(t)$', '$y_5(t)$']
     
-    """
-    Sinal composto por uma única frequência e caracterizado por uma função cosseno na forma 
-    y(t) = A * cos(2 * pi * F * t)
-    """
-    
-    def __init__(self, a, f, tw):
-        self.amplitude = a
-        self.frequencia = f
-        self.janela_tempo = tw
-        self.dt = tw/1000
-        
-        # eixo de tempo estendido, apenas para evitar artefatos nas extremidades das interpolações
-        self.tt_analogico = np.arange(-self.janela_tempo, (2*self.janela_tempo) + self.dt, self.dt)
-        
-        # Função de referência que caracteriza o sinal analógico na forma y(t) = A * cos(2 * pi * F * t).
-        # Pode ser amostrada em qualquer instante de tempo que se queira, de forma que esta é a melhor
-        # forma de representar um sinal contínuo para os objetivos desta demonstração.
-        self.funcao_cos = interp1d(self.tt_analogico, self.amplitude * \
-                                   np.cos(2*np.pi*self.frequencia*self.tt_analogico), kind='linear')      
-        
-        # Gera uma aproximação do sinal analógico calculando as amplitudes da função cosseno com inervalo
-        # de amostragem curto o suficiente para que ela possa ser visualizada como um sinal analógico contínuo.
-        self.analogico = self.funcao_cos(self.tt_analogico)
-        
-        # Inicializa com valores nulos pois não foi feita a amostragem ainda
-        self.dt_amostrado = None
-        self.tt_amostrado = None
-        self.amostrado = None
-        
-        # O sinal recuperado usa o mesmo dt do sinal analógico original, mas aqui inicializa com valor nulo.
-        self.recuperado = None
-        self.tt_recuperado = None
-
-   
-    def amostragem(self, f_am):
-        """
-        Faz a amostragem do sinal calculando as amplitudes da função cosseno de acordo com o intervalo
-        de amostragem definido.
-        """
-        
-        t_min = self.tt_analogico[0]
-        t_max = self.tt_analogico[-1]
-        self.dt_amostrado = 1/f_am
-        self.tt_amostrado = np.arange(t_min, t_max, self.dt_amostrado)
-
-        # amostragem das amplitudes do sinal analógico
-        self.amostrado = self.funcao_cos(self.tt_amostrado)      
+    for yn, c, l in zip(y, cores, labels):
+        plota_sinal(yn, ax=ax[0], 
+                    plt_kwargs={'color': c,
+                                'linewidth': 2.0,
+                                'label': l})
+    ax[0].set(xlabel='')
+    plota_sinal(sinal_analog, ax=ax[1], 
+                plt_kwargs={'color': 'blue',
+                            'linewidth': 2.0,
+                            'label': 'Sinal Analógico $s(t)$'})    
+    plt.tight_layout()
+    plt.show()
+    return None
 
 
-    def recupera(self):
-        """
-        Interpola as amplitudes que foram amostradas da função cosseno para demonstrar como seria o
-        comportamento real do sinal recuperado a partir destas amostras.
-        """
-        
-        t_min = np.min(self.tt_amostrado)
-        t_max = np.max(self.tt_amostrado)
-        self.tt_recuperado = np.arange(t_min, t_max + self.dt, self.dt)
-        
-        funcao_recuperado = interp1d(self.tt_amostrado, self.amostrado, kind='cubic')
-        self.recuperado = funcao_recuperado(self.tt_recuperado)
+def executa_exercício5(amplitude, frequencia, t_window, fam):
+    sinal_analog = gera_sinal_analogico(amplitude[0], frequencia[0], t_window)
+    
+    for a,f in zip(amplitude[1:], frequencia[1:]):
+        s = gera_sinal_analogico(a, f, t_window)
+        sinal_analog.y = sinal_analog.y + s.y
 
-        
-# funções para as figuras
+    sinal_dig = digitaliza_sinal(sinal_analog, fam)
 
-def plota_amostragem(sinal, n_fig='X'):
-    """
-    Plota as amplitudes amostradas em relação ao sinal analógico original
-    """
-        
-    fig, ax = plt.subplots(figsize=(15,3))
-    ax.plot(sinal.tt_analogico, sinal.analogico, '-r', label='Sinal original')
-    ax.plot(sinal.tt_amostrado, sinal.amostrado, 'ob', label='Amplitudes amostradas')
-    ax.set_xlabel("Tempo (s)", fontsize=14)
-    ax.set_ylabel("Amplitude (ua)", fontsize=14)
-    ax.set_title("Figura " + n_fig + ". Sinal amostrado a uma frequência de " + \
-                 str(1/sinal.dt_amostrado) + " amostras por segundo", fontsize=14)
-    ax.set_xlim([0, sinal.janela_tempo])
-    ax.set_ylim([np.min(sinal.analogico)*1.25, np.max(sinal.analogico)*1.25])
-    ax.grid()
-    ax.legend(loc='lower right', fontsize=12)
-
+    fig, ax = plt.subplots(figsize=(15, 4))
+    plota_sinal(sinal_analog, ax=ax, 
+                plt_kwargs={'color': 'blue',
+                            'linewidth': 2.0,
+                            'linestyle': '-',
+                            'alpha': 0.2,
+                            'label': 'Sinal Original'})
+    plota_sinal(sinal_dig, ax=ax, 
+                plt_kwargs={'markerfacecolor': 'red',
+                            'markeredgecolor': 'none',
+                            'marker': 'o',
+                            'markersize': 8.0,
+                            'linestyle': 'none',
+                            'label': 'Amplitude Amostrada'})
+    plota_sinal(sinal_dig, ax=ax, 
+                plt_kwargs={'color': 'red',
+                            'linestyle': '-',
+                            'linewidth': 2.0,
+                            'label': 'Sinal Recuperado'})
+    plt.tight_layout()
+    plt.show()
     
-def plota_analogico(sinal, n_fig='X'):
-    """
-    Plota a função cosseno calculada a intervalos de tempo pequenos o suficiente para que se possa 
-    fazer uma representação do sinal analógico original.
-    """
-    
-    fig, ax = plt.subplots(figsize=(15,3))
-    ax.plot(sinal.tt_analogico, sinal.analogico)
-    ax.set_xlabel("Tempo (s)", fontsize=14)
-    ax.set_ylabel("Amplitude (ua)", fontsize=14)
-    ax.set_title("Figura " + n_fig + ". Representação do sinal analógico original", fontsize=14)
-    ax.set_xlim([0, sinal.janela_tempo])
-    ax.set_ylim([np.min(sinal.analogico)*1.25, np.max(sinal.analogico)*1.25])
-    ax.grid()
-    
-    
-def plota_representacao(sinal, n_fig='X'):
-    """
-    Plota a interpolação do sinal feita a partir das amostras obtidas do sinal analógico.
-    """
-    
-    fig, ax = plt.subplots(figsize=(15,3))
-    ax.plot(sinal.tt_recuperado, sinal.recuperado, '--b', label='Sinal recuperado', linewidth=1)
-    ax.plot(sinal.tt_amostrado, sinal.amostrado, 'ob', label='Amplitudes amostradas')
-    ax.set_xlabel("Tempo (s)", fontsize=14)
-    ax.set_ylabel("Amplitude (ua)", fontsize=14)
-    ax.set_title("Figura " + n_fig + ". Sinal recuperado da amostragem", fontsize=14)
-    ax.set_xlim([0, sinal.janela_tempo])
-    ax.set_ylim([np.min(sinal.analogico)*1.25, np.max(sinal.analogico)*1.25])
-    ax.grid()
-    ax.legend(loc='lower right', fontsize=12)
-    
-    
-def plota_comparacao(sinal, n_fig='X'):
-    """
-    Plota a interpolação do sinal feita a partir das amostras obtidas do sinal 
-    analógico e a sobrepõe à representação do sinal analógico original.
-    """
-    
-    fig, ax = plt.subplots(figsize=(15,3))
-    ax.plot(sinal.tt_analogico, sinal.analogico, '-r', label='Sinal original')
-    ax.plot(sinal.tt_recuperado, sinal.recuperado, '--b', label='Sinal recuperado')
-    ax.set_xlabel("Tempo (s)", fontsize=14)
-    ax.set_ylabel("Amplitude (ua)", fontsize=14)
-    ax.set_title("Figura " + n_fig + ". Comparação entre o sinal original e o sinal"\
-                 " recuperado da amostragem", fontsize=14)
-    ax.set_xlim([0, sinal.janela_tempo])
-    ax.set_ylim([np.min(sinal.analogico)*1.25, np.max(sinal.analogico)*1.25])
-    ax.grid()
-    ax.legend(loc='lower right', fontsize=12)
-    
+    return None
