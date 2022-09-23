9c53b2c7a6ec986b4bd8c13cbe3cb51ff4df966a
Emerson LABGEOF <emerson.almeida.ufu@gmail.com>
2022-09-23 08:54:19 -0300

    adição de funções usadas no notebook sev_parte2

--- Geofisica2/modulos/er1d.py
+++ Geofisica2/modulos/er1d.py
@@ -134,6 +134,91 @@ class modelo_geo():
         plt.show()
 
 
+class curva_campo():
+    
+    """
+    Define o objeto referente à curva de campo, com as posições de eletrodos e 
+    valores de resistividade aparente. Possui um método para executar a modelagem
+    da curva
+    """
+    
+    def __init__(self, AB_campo, MN_campo, dados_medidos):
+        self.meioAB = AB_campo
+        self.meioMN = MN_campo
+        self.rho_a = dados_medidos
+        
+    def plota_dados(self):
+        
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.loglog(self.meioAB, self.rho_a, '.k', linewidth='0.0')
+        ax.set_xlabel('AB/2 (m)', fontsize='14')
+        ax.set_ylabel('Resistividade Aparente ($\Omega$.m)', fontsize='14')
+        ax.set_title('Dados de campo', fontsize='14')
+        ax.set_ylim(define_escala_y(self.rho_a))
+        ax.set_xlim([1, 1000])
+        ax.grid(which='both')
+
+        plt.show()
+        
+    def modela_dados(self, modelo):
+        
+        """
+        Modela os dados e plota a curva modelada junto com o modelo geoelétrico.
+        """
+        
+        sev_sintetica = sev(self.meioAB, self.meioMN, arranjo='Schlumberger')
+        sev_sintetica.fwd(modelo)
+        
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(15, 7))
+        
+        ax[0].loglog(sev_sintetica.meioAB, sev_sintetica.rho_a, '-r', label='Curva modelada')
+        ax[0].plot(self.meioAB, self.rho_a, '.k', label='Dados de campo')
+
+        rmspe = np.round(np.sqrt(np.mean(((self.rho_a - sev_sintetica.rho_a) / self.rho_a)**2, axis=0)) * 100, 2)
+        
+        erro_str = 'Erro RMS: ' + str(rmspe) + ' %'
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.9)
+        ax[0].text(2, 20, erro_str, fontsize=12, verticalalignment='center', bbox=props)
+        ax[0].set_xlabel('AB/2 (m)', fontsize='14')
+        ax[0].set_ylabel('Resistividade Aparente ($\Omega$.m)', fontsize='14')
+        ax[0].set_title('Curva modelada sobre os dados de campo', fontsize='14')
+        ax[0].set_ylim(define_escala_y(self.rho_a))
+        ax[0].set_xlim([1, 1000])
+        ax[0].legend(fontsize=14)
+        ax[0].grid(which='both')
+
+        # mesh = TensorMesh([np.r_[modelo.h, modelo.z_max - modelo.h.sum()]])
+        # plot_layer(modelo.model_map * modelo.rho, mesh, xlim=[1, 1e5], ax=ax[1], showlayers=False)
+        ax[1] = plot_1d_layer_model(modelo.h, modelo.model_map * modelo.rho)
+        ax[1].set_xlabel('Resistividade Real (Ohm.m)')
+        ax[1].set_ylabel('Profundidade (m)')
+        ax[1].set_title('Modelo de n=' + \
+                        str(len(modelo.rho)) + ' camadas', fontsize='14')
+        ax[1].grid(which='both')
+
+        plt.show()
+        
+        
+def define_escala_x(x):
+    minimo = 10**(np.floor(np.log10(np.min(x))))
+    maximo = 10**(np.ceil(np.log10(np.max(x))))
+    
+    return [minimo, maximo]
+
+
+def define_escala_y(d):
+    min_exp = np.floor(np.log10(np.min(d)))
+    max_exp = np.ceil(np.log10(np.max(d)))
+    
+    if np.log10(np.min(d)) - min_exp < 0.3:
+        min_exp -= 1
+
+    if max_exp - np.min(d) < 0.2:
+        max_exp += 1
+
+    return [10**min_exp, 10**max_exp]
+        
+        
 def calcula_tendencias(sondagem, modelo):
     """
     Calcula a tendência da curva de resistividade aparente para cada
